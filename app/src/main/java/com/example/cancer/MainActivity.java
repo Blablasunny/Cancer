8e5cdfa14545ad73a9ba46f8763652b88c5de746
Blablasunny <91792426+Blablasunny@users.noreply.github.com>
2022-02-21 21:06:58 +0300

    1, 2, 3, 4 экраны

--- app/src/main/java/com/example/cancer/MainActivity.java
+++ app/src/main/java/com/example/cancer/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.cancer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.group_4);
+        Button button2 = (Button) findViewById(R.id.group_5);
+        Button button3 = (Button) findViewById(R.id.group_6);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(i);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cancer/MainActivity3.java
+++ app/src/main/java/com/example/cancer/MainActivity3.java
@@ -2,13 +2,51 @@ package com.example.cancer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        ImageView imv = (ImageView) findViewById(R.id.img1);
+        imv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        Bitmap bitmap = null;
+        ImageView imv = (ImageView) findViewById(R.id.img1);
+
+        switch (requestCode) {
+            case GALLERY_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    imv.setImageBitmap(bitmap);
+                }
+        }
     }
 }
